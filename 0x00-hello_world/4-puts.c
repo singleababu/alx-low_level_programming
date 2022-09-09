aaaff78b59d8c7df9244f27c7bebbbb85dbfe922
singleababu <singleababu26@gmail.com>
2022-09-09 05:14:49 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 
-/''
-' main . print the string in the put function
-'
-'Description: using the main function
-'this program prints 'programing is like building a multilingual puzzle
-'Return: 0
-'/
+#include <stdio.h>
+/**
+* main . print the string in the put function
+*
+*Description: using the main function
+*this program prints 'programing is like building a multilingual puzzle
+*Return: 0
+*/
 int main(void)
 {
     puts('\'programing is like building a multilingual puzzle');
