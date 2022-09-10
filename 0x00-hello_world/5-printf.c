5164ec46dbe61aab0de1cde80b80d10c0e8017f4
singleababu <singleababu26@gmail.com>
2022-09-09 23:14:47 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,2 @@
-#include <stdio.h>
-/**
-* main . print the string in the put function
-*
-*Description: using the main function
-*this program prints 'programing is like building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
-{
-    puts('\'programing is like building a multilingual puzzle');
-    return (0);
-}
+#!/bin/bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
