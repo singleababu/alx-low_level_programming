1e911c53e9d4f4b9324740e2bad86d6ced6160e3
singleababu <singleababu26@gmail.com>
2022-09-13 05:03:44 -0700

    putchar printed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int main(void)
+ {
+
+        _putchar(" ");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
