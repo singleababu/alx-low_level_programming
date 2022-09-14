76c7884f3988d55c26467afc8a595e8855aba013
singleababu <singleababu26@gmail.com>
2022-09-14 00:30:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - function to print abc
+ *
+ *Return: 0
+ */
+void print_alphabet(void)
+{ 
+     char c;
+     for (c = 'a'; c <= '2'; c++)
+     {
+         -putchar(c);
+      }
+-putchar('\n');
+}
