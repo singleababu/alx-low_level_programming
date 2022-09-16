06fda339290f25135685965a64ed686d209e2a7f
singleababu <singleababu26@gmail.com>
2022-09-16 03:32:19 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,7 +6,11 @@
 
 /**
 
- * comments goes here
+ * main - finds and prints the largest prime factor of the number 612852475143
+
+ * followed by a new line
+
+ * Return: Always 0 (Success)
 
  */
 
@@ -14,27 +18,57 @@ int main(void)
 
 {
 
-	int c;
+	long int n;
 
-	long num = 612852475143;
+	long int max;
+
+	long int i;
 
 
 
-	for (c = (int) sqrt(num); c > 2; c++)
+	n = 612852475143;
+
+	max = -1;
+
+
+
+	while (n % 2 == 0)
 
 	{
 
-		if (num % c == 0)
+		max = 2;
+
+		n /= 2;
+
+	}
+
+
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+
+	{
+
+		while (n % i == 0)
 
 		{
 
-			printf("%d\n", c);
+			max = i;
 
-			break;
+			n = n / i;
 
 		}
 
 	}
+
+
+
+	if (n > 2)
+
+		max = n;
+
+
+
+	printf("%ld\n", max);
 
 
 
