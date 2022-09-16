ea53c918d29cd962d6a4f006d7488c62a924e306
singleababu <singleababu26@gmail.com>
2022-09-16 01:09:53 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,37 +1,19 @@
 #include "main.h"
-
 /**
+ * print_most_numbers -print number
  *
- *  * print_most_numbers -print number
- *
- *   *
- *
- *    * return: alway 0.
- *
- *     */
-
+ * return: alway 0.
+ */
 void print_most_numbers(void)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 0; i < 10; i++)
-
-					{
-
-								if (i != 2 && i != 4)
-
-											{
-
-															_putchar (i + '0');
-
-																	}
-
-									}
-
-				_putchar ('\n');
-
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar (i + '0');
+		}
+	}
+	_putchar ('\n');
 }
